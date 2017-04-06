2db2a5dccdd5f4e354cd204e99407092dc412c70
SamDC <sam.davis.castro@gmail.com>
2017-04-06 11:21:24 -0400

    Organized comparison operators reduced DOM function calls

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,21 +4,24 @@ var pxFromTop = 0;
 var windWidth;
 
 function fixNavTop() {
+    console.log('fix called');
     navbar.className = 'navbar navbar-inverse navbar-fixed-top';
     main.className = 'scrollMargin';
 }
+
 function unfixNav() {
+    console.log('unfix called');
     navbar.className = 'navbar navbar-inverse';
     main.className = '';
 }
 window.addEventListener('scroll', function(e) {
     pxFromTop = window.scrollY;
     windWidth = window.innerWidth;
-    if ((windWidth > 767 && pxFromTop > 120) || (windWidth <= 767 && pxFromTop > 305)) {
+    if (((windWidth > 767 && pxFromTop > 120) || (windWidth <= 767 && pxFromTop > 305)) && (navbar.className === 'navbar navbar-inverse')) {
         window.requestAnimationFrame(function() {
             fixNavTop();
         });
-    } else if ((windWidth > 767 && pxFromTop <= 120) || (windWidth <= 767 && pxFromTop <= 305)) {
+    } else if (((windWidth > 767 && pxFromTop <= 120) || (windWidth <= 767 && pxFromTop <= 305)) && (navbar.className === 'navbar navbar-inverse navbar-fixed-top')) {
         window.requestAnimationFrame(function() {
             unfixNav();
         });
